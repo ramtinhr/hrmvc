53716278fa11c6ed080b417438028a0e791838ac
ramtin <ramtin.hrezaei@gmail.com>
2020-10-17 01:19:47 +0330

    finally fix webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// Webpack uses this to work with directories
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const config = {
+  module: {
+  }
+};
+let jsConfig = Object.assign({}, config, {
+  name: "js",
+  entry: "./public/resources/js/app.js",
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'js/app.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/app.css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+});
+module.exports = [
+  jsConfig
+]
