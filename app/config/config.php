94f7e30748a74c2375605d9aec7c451e4f928a9c
ramtin <ramtin.hrezaei@gmail.com>
2020-10-17 19:53:16 +0330

    changes

--- app/config/config.php
+++ app/config/config.php
@@ -2,9 +2,14 @@
   use Symfony\Component\Dotenv\Dotenv;
 
   /*
-   * vendor autoloader and vendor path
+   * Define vendor constant before requiring autoload
+   *
    */
   define('DIR_VENDOR', dirname(dirname(__DIR__)) . '/vendor');
+
+  /*
+   * vendor autoloader and vendor path
+   */
   if (file_exists(DIR_VENDOR . '/autoload.php')) {
     require_once(DIR_VENDOR . '/autoload.php');
   }
