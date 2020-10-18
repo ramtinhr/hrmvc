a9b97fa0a50979d7619aadc0a2f96b648c65c352
ramtin <ramtin.hrezaei@gmail.com>
2020-10-18 23:55:58 +0330

    fix some bugs in index page

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,4 +1,4 @@
 <?php require APP_ROOT . '/views/includes/header.php'; ?>
-  <h1><?php echo $data[title] ?></h1>
+  <h1><?php echo $data['title'] ?></h1>
   <p>My first PHP MVC framework to be PRO in php</p>
 <?php require APP_ROOT . '/views/includes/footer.php'; ?>
